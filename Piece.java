d31474c1a0fdc771ecd2cf2cb89867a1947bab50
Jeffrey Chan <superjeffreyc@yahoo.com>
2016-02-13 17:19:47 -0500

    Removed Player variable

--- Piece.java
+++ Piece.java
@@ -3,14 +3,6 @@ public class Piece {
 	Player player;
 	int location = 0;
 
-	Piece(Player player) {
-		this.player = player;
-	}
-
-	public void getPlayer() {
-		return this.player;
-	}
-
 	public void setLocation(int location) {
 		this.location = location;
 	}
