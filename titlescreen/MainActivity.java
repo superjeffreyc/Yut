2abea322e8f5dc667fbd03b23681728aa8b70899
atsukocacola@gmail.com <Atsukocacola@gmail.com>
2016-02-14 01:36:44 -0500

    added game board

--- titlescreen/MainActivity.java
+++ titlescreen/MainActivity.java
@@ -1,77 +1,71 @@
-package com.example.atsukoshimizu.yutnori;
+package com.jeffreychan.yutnori;
+
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.example.atsukoshimizu.yutnori.R;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.titlescreen);
+public class MainActivity extends Activity implements OnClickListener {
 
-        //Penguin jump animation
-        ImageView penguinJumpImageView = (ImageView)findViewById(R.id.penguinjumpimageview);
-        penguinJumpImageView.setBackgroundResource(R.drawable.penguinjumpanimation);
-        AnimationDrawable penguinJumpAnimation = (AnimationDrawable) penguinJumpImageView.getBackground();
-        penguinJumpAnimation.start();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.titlescreen);
+		ImageView penguinJumpImageView = (ImageView) findViewById(R.id.penguinjumpimageview);
+		penguinJumpImageView.setBackgroundResource(R.drawable.penguinjumpanimation);
+		AnimationDrawable penguinJumpAnimation = (AnimationDrawable) penguinJumpImageView.getBackground();
+		penguinJumpAnimation.start();
 
-        //Seal move animation
-        ImageView sealMoveImageView = (ImageView)findViewById(R.id.sealmoveimageview);
-        sealMoveImageView.setBackgroundResource(R.drawable.sealmoveanimation);
-        AnimationDrawable sealMoveAnimation = (AnimationDrawable) sealMoveImageView.getBackground();
-        sealMoveAnimation.start();
+		ImageView sealJumpImageView = (ImageView) findViewById(R.id.sealmoveimageview);
+		sealJumpImageView.setBackgroundResource(R.drawable.sealmoveanimation);
+		AnimationDrawable sealJumpAnimation = (AnimationDrawable) sealJumpImageView.getBackground();
+		sealJumpAnimation.start();
 
-        penguinJumpImageView.setX(200f);
+		ImageView background = (ImageView) findViewById(R.id.titleBackground);
+		background.setBackgroundResource(R.drawable.background);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+		Button startButton = (Button) findViewById(R.id.startButton);
+		startButton.setOnClickListener(this);
+		startButton.setX(300f);
+		startButton.setY(500f);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("ActioTn", null).show();
-            }
-        });
-    }
+		sealJumpImageView.setX(200f);
 
-    public void startbutton(View view)
-    {
-        Intent intent = new Intent(this, ToActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-    }
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_board, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public void onClick(View v) {
+		startActivity(new Intent(this, BoardActivity.class));
+	}
 }
