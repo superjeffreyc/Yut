9ec29f1ff4baa08c9269510f649d9f700270831e
Jeffrey Chan <superjeffreyc@yahoo.com>
2016-03-17 22:09:52 -0400

    Modified handleMoveset to calculate all possible moves

--- Yunnori/app/src/main/java/com/jeffreychan/yutnori/Piece.java
+++ Yunnori/app/src/main/java/com/jeffreychan/yutnori/Piece.java
@@ -28,91 +28,80 @@ public class Piece {
 		return this.location;
 	}
 
-	public void handleMovement(int moves){
-		if (specialTiles.contains(new Integer(location))){
-			if (location == 0){
-				if (moves >= 1) {
-					location = 1 + moves;
+	public int[] calculateMoveset(int[] moves){
+
+		int[] possibleMoves = new int[moves.length];
+
+		for (int i = 0; i < moves.length; i++) {
+			if (specialTiles.contains(new Integer(location))) {
+				if (location == 0) {
+					if (moves[i] >= 1) {
+						location = 1 + moves[i];
+					}
+				} else if (location == 1) {
+					if (moves[i] >= 1) {
+						location = 1 + moves[i];
+					} else if (moves[i] == -1) {
+						location = 32;
+					}
+				} else if (location == 6) {
+					if (moves[i] >= 1) {
+						location = 20 + moves[i];
+					} else {
+						location--;
+					}
+				} else if (location == 11) {
+					if (moves[i] >= 1) {
+						location = 25 + moves[i];
+					} else {
+						location--;
+					}
+				} else if (location == 21) {
+					if (moves[i] >= 1 && moves[i] <= 4) {
+						location = 21 + moves[i];
+					} else if (moves[i] == -1) {
+						location = 6;
+					} else if (moves[i] == 5) {
+						location = 16;
+					}
+				} else if (location == 22) {
+					if (moves[i] >= 1 && moves[i] <= 3) {
+						location = 22 + moves[i];
+					} else if (moves[i] == -1) {
+						location--;
+					} else if (moves[i] == 4) {
+						location = 16;
+					} else if (moves[i] == 5) {
+						location = 17;
+					}
+				} else if (location == 23) {
+					location = 28 + moves[i];
+				} else if (location == 24) {
+					if (moves[i] == 1) {
+						location = 25;
+					} else if (moves[i] == -1) {
+						location--;
+					} else if (moves[i] >= 2) {
+						location = 14 + moves[i];
+					}
+				} else if (location == 25) {
+					if (moves[i] >= 1) {
+						location = 15 + moves[i];
+					} else if (moves[i] == -1) {
+						location--;
+					}
 				}
+			} else {
+				location += moves[i];
 			}
-			else if (location == 1){
-				if (moves >= 1){
-					location = 1 + moves;
-				}
-				else if (moves == -1){
-					location = 32;
-				}
+
+			if (location > 32) {
+				location = 32;
 			}
-			else if (location == 6){
-				if (moves >= 1) {
-					location = 20 + moves;
-				}
-				else {
-					location--;
-				}
-			}
-			else if (location == 11){
-				if (moves >= 1){
-					location = 25 + moves;
-				}
-				else {
-					location--;
-				}
-			}
-			else if (location == 21){
-				if (moves >= 1 && moves <= 4){
-					location = 21 + moves;
-				}
-				else if (moves == -1){
-					location = 6;
-				}
-				else if (moves == 5){
-					location = 16;
-				}
-			}
-			else if (location == 22){
-				if (moves >= 1 && moves <= 3){
-					location = 22 + moves;
-				}
-				else if (moves == -1){
-					location--;
-				}
-				else if (moves == 4){
-					location = 16;
-				}
-				else if (moves == 5){
-					location = 17;
-				}
-			}
-			else if (location == 23){
-				location = 28 + moves;
-			}
-			else if (location == 24){
-				if (moves == 1){
-					location = 25;
-				}
-				else if (moves == -1){
-					location--;
-				}
-				else if (moves >= 2) {
-					location = 14 + moves;
-				}
-			}
-			else if (location == 25){
-				if (moves >= 1){
-					location = 15 + moves;
-				}
-				else if (moves == -1){
-					location--;
-				}
-			}
-		} else {
-			location += moves;
+
+			possibleMoves[i] = location;
 		}
 
-		if (location > 32) {
-			location = 32;
-		}
-		
+		return possibleMoves;
 	}
 }
